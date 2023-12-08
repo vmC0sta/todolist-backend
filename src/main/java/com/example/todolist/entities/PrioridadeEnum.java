e02ab86f0effc17ced0b4c3c4e6ecd9cb267baeb
vmC0sta <duda.cv2404@gmail.com>
2023-12-08 13:24:37 -0300

    Excluído métodos desnecessários dos Enum

--- src/main/java/com/example/todolist/entities/PrioridadeEnum.java
+++ src/main/java/com/example/todolist/entities/PrioridadeEnum.java
@@ -1,21 +1,7 @@
 package com.example.todolist.entities;
 
 public enum PrioridadeEnum {
-    BAIXA(1),
-    MEDIA(2),
-    ALTA(3);
-    private int valor;
-    PrioridadeEnum(int valor) {
-    }
-    public int getValor() {
-        return valor;
-    }
-    public static PrioridadeEnum valueOf(int valor) {
-        for (PrioridadeEnum prioridadeEnum : values()) {
-            if (prioridadeEnum.valor == valor) {
-                return prioridadeEnum;
-            }
-        }
-        throw new IllegalArgumentException("Valor inválido: " + valor);
-    }
+    BAIXA,
+    MEDIA,
+    ALTA;
 }
--- src/main/java/com/example/todolist/entities/RealizadoEnum.java
+++ src/main/java/com/example/todolist/entities/RealizadoEnum.java
@@ -1,20 +1,6 @@
 package com.example.todolist.entities;
 
 public enum RealizadoEnum {
-    NAO(0),
-    SIM(1);
-    private int valor;
-    RealizadoEnum(int valor) {
-    }
-    public int getValor() {
-        return valor;
-    }
-    public static RealizadoEnum valueOf(int valor) {
-        for (RealizadoEnum realizadoEnum : values()) {
-            if (realizadoEnum.valor == valor) {
-                return realizadoEnum;
-            }
-        }
-        throw new IllegalArgumentException("Valor inválido: " + valor);
-    }
+    NAO,
+    SIM;
 }
